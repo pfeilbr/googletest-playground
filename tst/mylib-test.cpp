1b2626f4c21528238c55ffafb1f7c379db8060de
Brian Pfeil <brian.pfeil@gmail.com>
2020-01-04 15:47:51 -0500

    added test fixture example

--- tst/mylib-test.cpp
+++ tst/mylib-test.cpp
@@ -21,6 +21,8 @@ static void print_cpp_version()
     std::cout << "C++ Version: " << get_cpp_version() << "\n";
 }
 
+// ---
+
 TEST(MyLib, adhoc)
 {
     print_cpp_version();
@@ -42,4 +44,57 @@ TEST(MyLib, multiply)
     EXPECT_EQ(MyLib::multiply(0, 1), 0);
     EXPECT_EQ(MyLib::multiply(1, 1), 1);
     EXPECT_EQ(MyLib::multiply(10, 10), 100);
+}
+
+// ---
+
+// test fixture example
+
+class FooTest : public ::testing::Test
+{
+protected:
+    // You can remove any or all of the following functions if their bodies would
+    // be empty.
+
+    FooTest()
+    {
+        // You can do set-up work for each test here.
+    }
+
+    ~FooTest() override
+    {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    void SetUp() override
+    {
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+        std::cout << "SetUp" << std::endl;
+    }
+
+    void TearDown() override
+    {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+        std::cout << "TearDown" << std::endl;
+    }
+
+    // Class members declared here can be used by all tests in the test suite
+    // for Foo.
+};
+
+TEST_F(FooTest, MethodBarDoesAbc)
+{
+    EXPECT_EQ(0, 0);
+    EXPECT_EQ(1, 1);
+}
+
+TEST_F(FooTest, MethodBarDoesXyz)
+{
+    EXPECT_EQ(0, 0);
+    EXPECT_EQ(1, 1);
 }
